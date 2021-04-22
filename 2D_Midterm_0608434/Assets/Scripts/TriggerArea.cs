8ecdc8cecb83d13cf703a763f0077b76b8edf8f8
0608434 <71476500+0608434@users.noreply.github.com>
2021-04-23 06:05:34 +0800

    感應

---
+++ 2D_Midterm_0608434/Assets/Scripts/TriggerArea.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class TriggerArea : MonoBehaviour
+{
+    public GameObject[] stones;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "碰到")
+        {
+
+            stones[0].SetActive(false);
+
+        }
+
+    }
+
+}
